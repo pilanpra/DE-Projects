3af07e0cb2fad05ec83c6981108c129ff9e6d84b
Prasad Pilankar <123372942+pilanpra@users.noreply.github.com>
2024-04-03 04:06:29 -0700

    Terraform

---
+++ Bootcamp2024Project/terraform/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_version = ">= 1.0"
+  backend "gcs" {}  
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project
+  region = var.region
+  // credentials = file(var.credentials)  # or setup GOOGLE_APPLICATION_CREDENTIALS
+}
+
+# Data Lake Bucket
+resource "google_storage_bucket" "data-lake-bucket" {
+  name          = "${local.data_lake_bucket}_${var.project}" # Concatenating DL bucket & Project name for unique naming
+  location      = var.region
+
+  # Optional, but recommended settings:
+  storage_class = var.storage_class
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled     = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30  // days
+    }
+  }
+
+  force_destroy = true
+}
+
+# Data Warehouse Bigquery
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id = var.BQ_DATASET
+  project    = var.project
+  location   = var.region
+}
